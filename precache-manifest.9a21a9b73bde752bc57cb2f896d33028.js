9bbd6a1fd10bb93e0fd1f64e9523a6bda34fc589
Tarik Crnovrsanin <turokhunter@gmail.com>
2020-08-21 10:33:56 -0700

    Updates

---
+++ precache-manifest.9a21a9b73bde752bc57cb2f896d33028.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f68cdae127d2e08f3e5b2ce7cf17285e",
+    "url": "/Turokhunter/Menu-Creator/index.html"
+  },
+  {
+    "revision": "f4d7f673b7cb5e2a5685",
+    "url": "/Turokhunter/Menu-Creator/static/css/2.ca784722.chunk.css"
+  },
+  {
+    "revision": "f05fa0f88c9bdad8171a",
+    "url": "/Turokhunter/Menu-Creator/static/css/main.96a3209a.chunk.css"
+  },
+  {
+    "revision": "f4d7f673b7cb5e2a5685",
+    "url": "/Turokhunter/Menu-Creator/static/js/2.0e38a8bb.chunk.js"
+  },
+  {
+    "revision": "5ad387cd57eb31f6f50768be47fe3b09",
+    "url": "/Turokhunter/Menu-Creator/static/js/2.0e38a8bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f05fa0f88c9bdad8171a",
+    "url": "/Turokhunter/Menu-Creator/static/js/main.279cae3c.chunk.js"
+  },
+  {
+    "revision": "aaf3c6bdd9103104749b",
+    "url": "/Turokhunter/Menu-Creator/static/js/runtime-main.ba05d7d6.js"
+  },
+  {
+    "revision": "3df96e6ba29ce750cc23f37107aba2c1",
+    "url": "/Turokhunter/Menu-Creator/static/media/delete.3df96e6b.png"
+  }
+]);
